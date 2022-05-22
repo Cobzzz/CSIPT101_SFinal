72414b8dd9739e3b6fd5fb36aacd454496afe9c8
Cobbie Andrei de los Reyes <cobbiedelosreyes17@gmail.com>
2022-05-22 17:12:55 +0800

    Created an API

--- FoodWareHouseAPI/models.py
+++ FoodWareHouseAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class foods(models.Model):
+    Mfd_id= models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    UnitPrice = models.DecimalField(max_digits=5,decimal_places=2)
+    UnitsInStock = models.PositiveSmallIntegerField()
